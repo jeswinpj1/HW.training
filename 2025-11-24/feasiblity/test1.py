2285d01e9f1825c64bff55cc879e2ad4180ebc15
jeswin <jeswinpj50@gmail.com>
2025-11-24 17:56:14 +0530

    new log update

--- 2025-11-24/feasiblity/test1.py
+++ 2025-11-24/feasiblity/test1.py
@@ -1,1 +1,17 @@
 https://www.jarir.com/
+
+import requests
+
+# Replace with your desired URL
+url = "https://www.homecentre.com/ae"
+
+try:
+    response = requests.get(url)
+    response.raise_for_status
+    print("\nResponse Text:\n")
+    print(response.text)
+    print("\nStatus Code:", response.status_code)
+except requests.exceptions.RequestException as e:
+    print("Error occurred:", e)
+
+
